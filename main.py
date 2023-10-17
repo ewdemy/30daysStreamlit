6cf50838f0d4a7c2db1052d2ba67f034a3372218
ewdemy <ewdemy@hotmail.com>
2023-10-17 16:05:15 -0300

    Dia 05

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import streamlit as st
+import pandas as pd
+import altair as alt
+import numpy as np
 
-st.header("st.button")
+st.header("st.write")
+st.write("Hello *world* :sunglasses:")
+st.write(12345)
 
-if st.button("Say Hello"):
-    st.write("Why hello there")
-else:    
-    st.write("Good Bye!")
+df = pd.DataFrame({
+    "First Column": [1,2,3,4,5],
+    "Second Column": ["A","B","C","D","E"]
+})
+
+st.write(df)
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+st.write(df2)
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
